a957414248b32715176bbac4a4ed7b2b565de676
saasbook <saasbook@saasbook.(none)>
2013-10-05 14:46:20 -0700

    Many blocks size, defines

--- main.cpp
+++ main.cpp
@@ -1,4 +1,15 @@
-#define BLOCK_SIZE 2 * 1024 * 1024
+#define BLOCK_SIZE_64KB 64 * 1024
+#define BLOCK_SIZE_128KB 128 * 1024
+#define BLOCK_SIZE_256KB 256 * 1024
+#define BLOCK_SIZE_512KB 512 * 1024
+#define BLOCK_SIZE_1MB 1024 * 1024
+#define BLOCK_SIZE_2MB 2 * 1024 * 1024
+#define BLOCK_SIZE_4MB 4 * 1024 * 1024
+#define BLOCK_SIZE_8MB 8 * 1024 * 1024
+#define BLOCK_SIZE_16MB 16 * 1024 * 1024
+#define BLOCK_SIZE_32MB 32 * 1024 * 1024
+#define BLOCK_SIZE_64MB 64 * 1024 * 1024
+#define BLOCK_SIZE_128MB 128 * 1024 * 1024
 
 #include <stxxl.h>
 #include <syscall.h>
@@ -10,9 +21,6 @@ using stxxl::file;
 using std::cin;
 using std::cout;
 using std::endl;
-
-typedef stxxl::VECTOR_GENERATOR<float, 4, 8, BLOCK_SIZE>::result vector_type;
-typedef vector_type::const_iterator c_it;
 
 
 class Profiler
@@ -44,18 +52,19 @@ private:
 void generateBigFile(const char * filename) {
 
     syscall_file file(filename, file::WRONLY);
-    vector_type v_write (&file);
-    
+    stxxl::vector<float> v_write (&file);
+
     for (int i = 0; i < 1000000000; ++i) {
       v_write.push_back(1.0);
     }
 }
 
+template <typename VectorType>
 double getSum(const char * filename) {
   syscall_file file(filename, file::RDONLY);
-  vector_type v_read(&file);
+  VectorType v_read(&file);
   double result = 0;
-
+  typedef typename VectorType::const_iterator c_it;
   for (c_it it = v_read.begin(); it != v_read.end(); ++it) {
     result += *it;
   }
@@ -64,9 +73,16 @@ double getSum(const char * filename) {
 }
 
 int main() {
-  {
-    Profiler profiler;
-    cout << getSum("bigFile.txt") << endl;
+  #define RUN(SIZE) \
+  {\
+    typedef stxxl::VECTOR_GENERATOR<float, 4, 8, BLOCK_SIZE_##SIZE>::result vector_type;\
+    cout << #SIZE << endl;\
+    Profiler profiler;\
+    cout << getSum<vector_type>("bigFile.txt") << endl;\
   }
+  RUN(2MB)
+  RUN(4MB)
+  RUN(8MB)
+  
 }
 
